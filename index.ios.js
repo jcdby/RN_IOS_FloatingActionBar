f5dd4900098edb161c9c27e88096a2f41f8350d9
Ethan Li <jcdby@chemessen.com>
2016-03-28 13:13:51 +0900

    Update index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -9,11 +9,15 @@ import React, {
   ListView,
   Text,
   View,
-  TouchableHighlight
+  TouchableHighlight,
+  Dimensions,
+  StyleSheet
 } from 'react-native';
 
 let items = ['bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla','bla',]
 let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+let dimensions = Dimensions.get('window');
+
 
 class floatButton extends Component {
   constructor(props) {
@@ -28,18 +32,31 @@ class floatButton extends Component {
       <View style={{flex: 1}}>
         <ListView style={{flex: 1}}dataSource={this.state.datasource} renderRow={(rowData) => <Text>{rowData}
         </Text>}/>
-        <View style={{flex: 0.001,top: -60, alignSelf: 'flex-end', justifyContent: 'center', alignItems: 'center'}}>
-          <TouchableHighlight style={{ backgroundColor: 'red', width: 50, height: 50, borderRadius: 25,justifyContent: 'center', alignItems: 'center', margin: 20}} onPress={()=>{console.log('touched');}}  >
+          <TouchableHighlight style={styles.floatingButton} onPress={()=>{console.log('touched');}}  >
             <Text>
               +
             </Text>
           </TouchableHighlight>
-        </View>
       </View>
 
     );
   }
 }
+
+let styles = StyleSheet.create({
+  floatingButton: {
+    position:'absolute',
+    top: dimensions.height - 100,
+    left: dimensions.width - 100,
+    borderRadius: 25,
+    width:50,
+    height:50,
+    backgroundColor: '#3668ba',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
 
 
 AppRegistry.registerComponent('floatButton', () => floatButton);
